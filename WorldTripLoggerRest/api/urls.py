d7130f1ee157ce1c0afcd70370e667d29263b901
Roberto Calvo <rocapal@gmail.com>
2013-08-05 15:25:40 +0000

    added rest call to getTraces (data and image)

--- WorldTripLoggerRest/api/urls.py
+++ WorldTripLoggerRest/api/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
     (r'^start/$', 'WorldTripLoggerRest.api.views.start'), 
     (r'^stop/$', 'WorldTripLoggerRest.api.views.stop'),
     (r'^status/$', 'WorldTripLoggerRest.api.views.status'),
+    (r'^traces/list/$', 'WorldTripLoggerRest.api.views.traces_list'),
+    (r'^traces/data/image/(?P<trace>.*)/$', 'WorldTripLoggerRest.api.views.trace_image'),
+    (r'^traces/data/(?P<trace>.*)/$', 'WorldTripLoggerRest.api.views.trace_data'),
+
     #(r'^config/photo/set/$', 'WorldTripLoggerRest.api.views.config_photo_set'),
     #(r'^config/video/set/$', 'WorldTripLoggerRest.api.views.config_video_set'),
 
--- WorldTripLoggerRest/api/views.py
+++ WorldTripLoggerRest/api/views.py
@@ -6,6 +6,7 @@ import subprocess
 
 from django.http import HttpResponse, HttpResponseServerError
 from django.utils import simplejson
+from django.core.servers.basehttp import FileWrapper
 from parser import *
 import jsonpickle
 
@@ -13,6 +14,7 @@ import jsonpickle
 JSON_MIMETYPE="application/json"
 STATUS_CMD="svstat" 
 STATUS_PATH="/home/pi/WTL/WTLCore/scripts/"
+TRACES_PATH="/home/pi/WTLv2-data/"
 
 def launch_cmd (command):
     code = call (command, shell=True)
@@ -58,10 +60,81 @@ def status (request):
     else:
         msg = "WorldTripLogger is stopped"
 
+
+    proc = subprocess.Popen(["du", "-sh","/home/pi/WTLv2-data/"],stdout=subprocess.PIPE)
+    line = proc.stdout.readline()
+    size = line.split("\t")[0]
+
+
+    proc = subprocess.Popen(["df", "-h"],stdout=subprocess.PIPE)
+    line = proc.stdout.readline()
+    line = proc.stdout.readline()
+    free_size = line.split(" ")[14]
+    porcent_free_size =line.split(" ")[16]
+
     data = {'code': code,
-            'message': msg}
+            'message': msg,
+            'size': size,
+            'free_size': free_size,
+            'porcent_free_size': porcent_free_size }
 
     return HttpResponse(simplejson.dumps(data), JSON_MIMETYPE)
 
     
+def traces_list (request):
+
+    files = os.listdir(TRACES_PATH)
+    ltraces = []
+    for trace in files:
+        if (trace[len(trace)-1] == 'D'):
+            ltraces.append(trace[:len(trace)-1])
+        
+        
+    ltraces = sorted(ltraces,reverse=True)
+    data = {'code': 1,
+            'traces': ltraces[0:20] }
+
+    return HttpResponse(simplejson.dumps(data), JSON_MIMETYPE)
+
+
+def trace_data (request, trace):
+
+
+    try:
+        data_file = open(TRACES_PATH + trace + "D")
+        lines = data_file.readlines()
     
+        data = {'code': 1,
+                'datetime' : lines[0].strip(),
+                'latitude' : lines[1].strip(),
+                'longitude': lines[2].strip(),
+                'altitude' : lines[3].strip(),
+                'speed'    : lines[4].strip(),
+                'temp'     : lines[5].strip(),
+                'image'    : trace}
+
+        return HttpResponse(simplejson.dumps(data), JSON_MIMETYPE)
+
+    except IOError:
+        
+        data = {'code'   : 0,
+                'message': trace + " file doesn't exist" }
+
+        return HttpResponseServerError(simplejson.dumps(data), JSON_MIMETYPE)
+
+
+def trace_image (request, trace):
+
+    try:
+        image_path = TRACES_PATH + trace + "I"
+        image_file = open (image_path)
+        response = HttpResponse(FileWrapper(image_file), content_type='image/jpeg')
+        response['Content-Disposition'] = 'attachment; filename=' + trace + '.jpg'
+        return response
+
+    except IOError:
+
+        data = {'code'   : 0,
+                'message': trace + " file doesn't exist" }
+
+        return HttpResponseServerError(simplejson.dumps(data), JSON_MIMETYPE)
